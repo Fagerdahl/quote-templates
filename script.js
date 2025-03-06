f5e7007dbecf4f1c25b75d3fac607ab4122204ef
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-03-06 23:46:40 +0100

    Picking only one quote with math functions

--- script.js
+++ script.js
@@ -2,13 +2,21 @@
 
 let apiQuotes = []; //Let because it is an empty array, but later it will change to a list of quotes
 
+//Show new quote button
+function newQuote(){
+    //Pick one random quote from ApiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+
 // // Get Quotes From API
 async function getQuotes() {
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    console.log(apiQuotes);
+    newQuote();
   } catch (error) {
     //errorhandling
   }
