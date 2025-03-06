a783e1ff8ad42ce8960115d7d9b886f9d0eb85df
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-03-06 23:52:02 +0100

    Pulling quotes locally instead of from API

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 //All JS are by default in the window element (window.console.log)
 
-let apiQuotes = []; //Let because it is an empty array, but later it will change to a list of quotes
+// let apiQuotes = []; //Let because it is an empty array, but later it will change to a list of quotes
 
 //Show new quote button
 function newQuote(){
     //Pick one random quote from ApiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
     console.log(quote);
 }
 
 
-// // Get Quotes From API
-async function getQuotes() {
-  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (error) {
-    //errorhandling
-  }
-}
+// // // Get Quotes From API
+// async function getQuotes() {
+//   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     newQuote();
+//   } catch (error) {
+//     //errorhandling
+//   }
+// }
 
-//Load
-getQuotes();
+// //Load
+// getQuotes();
+ newQuote();
