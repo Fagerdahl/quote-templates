a31d5da297e168299418fd1990bfaacad2057424
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-03-07 00:28:26 +0100

    Quote generated dynamically on the page

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ This will be an asyncronous fetch request to a rest API.
 I also have a frontend/backup stash with quotes.
 
 SHIFT + ALT + F = Prettier formatting
+Mark the word you want to rename. Press Ctrl + Shift + L
+
+Different Option for API
+There is an alternative option for this project that does not require an API key - https://zenquotes.io/
+
+Sample Requests
+https://zenquotes.io/api/quotes - Generate a JSON array of 50 random quotes on each request
+ 
+https://zenquotes.io/api/today - Generate the quote of the day on each request
+ 
+https://zenquotes.io/api/random - Generate a random quote on each request
--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 //All JS are by default in the window element (window.console.log)
 
-// let apiQuotes = []; //Let because it is an empty array, but later it will change to a list of quotes
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const authorText = document.querySelector('#author');
+const facebookBtn = document.querySelector('#facebook');
+const newQuoteBtn = document.querySelector('#new-quote');
+
+
+
+let apiQuotes = []; //Let because it is an empty array, but later it will change to a list of quotes
 
 //Show new quote button
 function newQuote(){
     //Pick one random quote from ApiQuotes array
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
 
-// // // Get Quotes From API
-// async function getQuotes() {
-//   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
-//   try {
-//     const response = await fetch(apiUrl);
-//     apiQuotes = await response.json();
-//     newQuote();
-//   } catch (error) {
-//     //errorhandling
-//   }
-// }
+// // Get Quotes From API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //errorhandling
+  }
+}
 
-// //Load
-// getQuotes();
- newQuote();
+//Load
+getQuotes();
