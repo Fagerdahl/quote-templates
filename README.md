ebce72d0613636f8077b00953ca47e1a44ccd410
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-03-06 23:39:15 +0100

    Quotes are fetched from API

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Quote Generator
 This will be an asyncronous fetch request to a rest API. 
 I also have a frontend/backup stash with quotes.
+
+SHIFT + ALT + F = Prettier formatting
--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //All JS are by default in the window element (window.console.log)
+
+let apiQuotes = []; //Let because it is an empty array, but later it will change to a list of quotes
+
+// // Get Quotes From API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    console.log(apiQuotes);
+  } catch (error) {
+    //errorhandling
+  }
+}
+
+//Load
+getQuotes();
